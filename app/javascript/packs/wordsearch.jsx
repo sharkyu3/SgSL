88b34149fc6c471fd7b257205395c87a962298da
sharkyu3 <lowhuiyu@hotmail.com>
2019-09-15 13:52:24 +0800

    wordsearch - first layer check to only allow selection of immediate cells

--- app/javascript/packs/wordsearch.jsx
+++ app/javascript/packs/wordsearch.jsx
@@ -15,6 +15,9 @@ export default class WordSearch extends React.Component{
             congrats: "hide",
             restartclass: "hide",
             grid: null,
+            answer: [],
+            correctcounter: 0,
+            clicked: null,
             boardclass: "col-10 grid-holder"
         };
     }
@@ -30,8 +33,6 @@ export default class WordSearch extends React.Component{
     }
 
     startGame(){
-        console.log("let's start game", this.state.words);
-
         const options = {
           dictionary: this.state.words,
           upperCase: true
@@ -39,21 +40,56 @@ export default class WordSearch extends React.Component{
 
         var ws = new WordPuzzle(options);
 
+        var ans = ws.grid.map((row, index) => {
+            ws.grid[index].map((col, index) => {
+                this.state.answer.push(col)
+            })
+        })
         this.setState({grid: ws.grid, butclass: "hide", restartclass: "start-game"})
     }
 
-    render(){
-        console.log("word search render", this.state.grid);
+    checkWin(){
+        console.log("checking for win");
+        //check counter. if counter = 10, WINNNNN!
+    }
 
+    checkWord(){
+        if(this.state.clicked === null){
+            console.log("clicked is null")
+            this.setState({clicked: event.target.id})
+            var target = document.getElementById(event.target.id);
+            target.classList.add("selected")
+        }else if(parseInt(event.target.id) === parseInt(this.state.clicked) + 1 || parseInt(event.target.id) === parseInt(this.state.clicked) - 1 || parseInt(event.target.id) === parseInt(this.state.clicked) + 10 || parseInt(event.target.id) === parseInt(this.state.clicked) - 10 || parseInt(event.target.id) === parseInt(this.state.clicked) + 11 || parseInt(event.target.id) === parseInt(this.state.clicked) - 11 || parseInt(event.target.id) === parseInt(this.state.clicked) + 9 || parseInt(event.target.id) === parseInt(this.state.clicked) - 9){
+            this.setState({clicked: event.target.id});
+            var target = document.getElementById(event.target.id);
+            target.classList.add("selected")
+            console.log("highlighting",target);
+            console.log("check answer", this.state.answer[event.target.id]);
+        }
+
+        //highlight cell selected by changing its class
+        //change classname to selected
+        //push corresponding answer alphabet into temp array
+        //concat temp array to string
+        //check temp against this.state.words ALL LOWERCASE
+        //if match, correct, change word div to different color, strikethrough and show merdeaf applause, add 1 to correct counter
+        //if match, check for win
+        //if not match, shake screen and show alamak
+    }
+
+    render(){
+
+        console.log("inside render", this.state.answer)
         let array = [];
         let alphabetlink = [];
         let board = "";
         let wordsDisplay = "";
+        let answerBoard = []
 
         if (this.state.grid !== null){
             console.log("grid has been set");
-            board = this.state.grid.map((fixed, index) => {
-                this.state.grid[index].map((alp, index)=>{
+            board = this.state.grid.map((fixed, x) => {
+                this.state.grid[x].map((alp, y)=>{
                     this.state.alphabets.map(alphabets=> {
                         if(alphabets.name.toLowerCase() === alp.toLowerCase()){
                             array.push(alphabets.link)
@@ -62,14 +98,13 @@ export default class WordSearch extends React.Component{
                 })
             })
         }
-        console.log("does array work?", array)
 
         if (array.length > 0){
             wordsDisplay = this.state.words.map((word, index)=>{
                 return <div className="wordies" id={word} key={index}>{word.toUpperCase()}</div>
             })
             board = array.map((image, index)=>{
-                return <div className="alp-cell" id={index} key={index}><img className="pic-cell" src={image}/></div>
+                return <div className="alp-cell" id={index} key={index}><img className="pic-cell" id={index} src={image} onClick={()=>this.checkWord()}/></div>
             })
         }
 
